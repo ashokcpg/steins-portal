effc23d33ba92e804abc4cfbe42acf2704e544dd
Ashok Chapagai <chapagaiashok@gmail.com>
2024-01-27 14:09:31 -0500

    add: base setup for the project

---
+++ app/_components/Button/Button.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CustomButton = () => {
+  return (
+    <div>This is my button</div>
+  )
+}
+
+export default CustomButton
---
+++ app/signup/page.js
@@ -0,0 +1,10 @@
+import { SignupComponent } from '@/components/component/signup-component'
+import React from 'react'
+
+const SignupPage = () => {
+  return (
+    <SignupComponent />
+  )
+}
+
+export default SignupPage
